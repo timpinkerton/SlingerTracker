dd8befb9721d199a41caf4dba0fc4af06b0f32a4
Tim <timpinkerton@mail.com>
2018-02-17 17:50:23 -0800

    Adding repository and temporary data.

---
+++ SlingerTracker/App_Data/EntriesRepository.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SlingerTracker.Models;
+
+
+namespace SlingerTracker.Data
+{
+    public class EntriesRepository
+    {
+        // a list of sample entries
+        private static Entry[] _entries = new Entry[]
+        {
+            new Entry()
+            {
+
+                Id = 1,
+                Name = "Hammy",
+                Email = "ham@hammies.com",
+                Date = "Today",
+                Location = "Buttery",
+                UserReview = "Ohhhhh yeah super good!"
+            },
+            new Entry()
+            {
+
+                Id = 2,
+                Name = "Arnie",
+                Email = "arnie@hammies.com",
+                Date = "Yesterday",
+                Location = "Tiffany's",
+                UserReview = "Not good."
+            },
+            new Entry()
+            {
+
+                Id = 3,
+                Name = "Dusty",
+                Email = "dusty@hammies.com",
+                Date = "Friday",
+                Location = "Big Ed's",
+                UserReview = "The. Best."
+            }
+        };
+
+        //This will return a collection of entries
+        public Entry[] GetEntries()
+        {
+            return _entries; 
+        }
+
+        public Entry GetEntry(int id)
+        {
+            Entry entryToReturn = null;
+
+            foreach (var entry in _entries)
+            {
+                if (entry.Id == id)
+                {
+                    entryToReturn = entry;
+
+                    break; 
+                }
+            }
+
+            return entryToReturn; 
+        }
+    }
+}
---
+++ SlingerTracker/Models/Entry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SlingerTracker.Models
+{
+    public class Entry
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Date { get; set; }
+        public string Location { get; set; }
+        public string UserReview { get; set; }
+
+        public string DisplayText
+        {
+            get
+            {
+                return Name + " at " + Location;
+            }
+        }
+
+    }
+}
